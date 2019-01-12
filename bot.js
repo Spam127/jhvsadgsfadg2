85cf3762b05516e1c1ad5d84f48834a0f678fac4
Spam127 <45201082+Spam127@users.noreply.github.com>
2019-01-12 06:51:43 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-
-
-  client.on('message', message => {
-    var prefix = "!";
+client.on('message', message => {
+    var prefix = "$";
     
         if (message.author.id === client.user.id) return;
         if (message.guild) {
@@ -19,17 +13,6 @@ client.on('ready', () => {
     message.channel.send("**اكتب شي بعد الكوماند**");
     return;
     }
-
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`skran`);
-        console.log(`---------------`);
-      console.log(`ON ${client.guilds.size} Servers `);
-    console.log(`---------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setGame(`Vomlem `,"http://twitch.tv/y04zgamer")
-   client.user.setStatus("dnd")
-
             message.guild.members.forEach(m => {
        if(!message.member.hasPermission('ADMINISTRATOR')) return;
                 var bc = new Discord.RichEmbed()
@@ -41,9 +24,32 @@ client.on('ready', () => {
                 m.send(`${m}`,{embed: bc});
             });
         }
-        } else {
+        } else {https://pastebin.com/night_mode
             return;
         }
-    });  
-    
+    });
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`skran`);
+        console.log(`---------------`);
+      console.log(`ON ${client.guilds.size} Servers `);
+    console.log(`---------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setGame(`Vomlem `,"http://twitch.tv/y04zgamer")
+   client.user.setStatus("dnd")
+});
+
+client.on('message', message => {
+    let args = message.content.split(' ').slice(1).join(' ');
+    if (message.content.startsWith('$bc-bot')){ // البريفكس والامر
+    if(!message.author.id === '') return;
+    message.channel.sendMessage('جار ارسال الرسالة ✅')
+    client.users.forEach(m =>{
+    m.sendMessage(args)
+    })
+    }
+    });
+
+
 client.login(process.env.BOT_TOKEN);
