8641923dfe39267c319fc24e77d987640b2f5cee
Spam127 <45201082+Spam127@users.noreply.github.com>
2019-01-12 06:47:25 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,20 +5,11 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', message => {
-    var prefix = "$";
-    
-        if (message.author.id === client.user.id) return;
-        if (message.guild) {
-       let embed = new Discord.RichEmbed()
-        let args = message.content.split(' ').slice(1).join(' ');
-    if(message.content.split(' ')[0] == prefix + 'bc') {
-        if (!args[1]) {
-    message.channel.send("**f!bc <message>**");
-    return;
-    }
-    
 
+
+  client.on('message', message => {
+    var prefix = "!";
+    
         if (message.author.id === client.user.id) return;
         if (message.guild) {
        let embed = new Discord.RichEmbed()
@@ -29,7 +20,15 @@ client.on('message', message => {
     return;
     }
 
-
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`skran`);
+        console.log(`---------------`);
+      console.log(`ON ${client.guilds.size} Servers `);
+    console.log(`---------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setGame(`Vomlem `,"http://twitch.tv/y04zgamer")
+   client.user.setStatus("dnd")
 
             message.guild.members.forEach(m => {
        if(!message.member.hasPermission('ADMINISTRATOR')) return;
@@ -45,40 +44,6 @@ client.on('message', message => {
         } else {
             return;
         }
-    });
-    
-    
-    
-
-
-
-
-
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`skran`);
-        console.log(`---------------`);
-      console.log(`ON ${client.guilds.size} Servers `);
-    console.log(`---------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setGame(`Vomlem `,"http://twitch.tv/y04zgamer")
-   client.user.setStatus("dnd")
-});
-
-
-
-
-client.on('message', message => {
-    let args = message.content.split(' ').slice(1).join(' ');
-    if (message.content.startsWith('$bc-bot')){ // البريفكس والامر
-    if(!message.author.id === '') return;
-    message.channel.sendMessage('جار ارسال الرسالة ✅')
-    client.users.forEach(m =>{
-    m.sendMessage(args)
-    })
-    }
-    });
-
-    
+    });  
     
 client.login(process.env.BOT_TOKEN);
